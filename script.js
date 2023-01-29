81b1970ea5921a859f9b33a11a4299918611c367
Vishwas Malik <97444195+itsvishwasmalik@users.noreply.github.com>
2023-01-29 10:51:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const text = document.querySelector("#joke");
 var type = "random";
 
+const fetchAPI = () => {
+    return new Promise((resolve, reject) => {
+        var selectedCetegory = type.toLowerCase();
+        var url =
+            type === "random" || selectedCetegory === "random"
+                ? "https://api.chucknorris.io/jokes/random"
+                : "https://api.chucknorris.io/jokes/random?category=" +
+                  selectedCetegory;
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                if (data["value"]) {
+                    text.innerText = data["value"];
+                }
+            })
+            .catch("404");
+        resolve("Success");
+    });
+};
+
+const useAPI = async () => {
+    return fetchAPI();
+};
+
 var dropdownMenu = document.querySelector(".dropdown-menu");
 dropdownMenu.addEventListener("click", (e) => {
     e.stopPropagation();
@@ -11,21 +38,5 @@ dropdownMenu.addEventListener("click", (e) => {
 });
 
 document.querySelector("#get").addEventListener("click", () => {
-    var selectedCetegory = type.toLowerCase();
-    var url =
-        type === "random" || selectedCetegory === "random"
-            ? "https://api.chucknorris.io/jokes/random"
-            : "https://api.chucknorris.io/jokes/random?category=" +
-              selectedCetegory;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            if (data["value"]) {
-                text.innerText = data["value"];
-            }
-        })
-        .catch("404");
+    useAPI();
 });
